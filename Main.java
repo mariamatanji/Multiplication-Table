8c63ae2d23745debd80ae5e13ee9eff9c11d68d5
mariamatanji <38054680+mariamatanji@users.noreply.github.com>
2025-11-09 13:49:44 -0800

    Implement multiplication table in Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner input = new Scanner(System.in);
+	    int n = input.nextInt();
+	    
+	    for(int i =1; i<=12; i++){
+	        
+	        System.out.println(n + " x " + i + " = " + (n * i));
+	    }
+		
+	}
+}
